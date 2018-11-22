dd2f3c723082042259c5c65b54b157eba7e832c1
smeerws <hello@xrux.at>
2018-11-22 10:09:43 +0100

    init mygame folder, scripts folder, first version of .cs for input on right controller

---
+++ Assets/MyGame/Scripts/HandControllerInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandControllerInput : MonoBehaviour {
+
+    public SteamVR_TrackedObject tracktobj;
+    public SteamVR_Controller.Device device;
+
+	// Use this for initialization
+	void Start ()
+    {
+        tracktobj = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        device = SteamVR_Controller.Input((int)tracktobj.index);
+
+        if (device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+        {
+
+        }
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+
+        }
+	}
+}
